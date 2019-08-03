841cb77f853f302fd799337f6493d04dce488d49
Kennedy Kariuki <kennedy.kariuki@tracom.co.ke>
2019-08-03 18:45:27 +0300

    Dog Data

--- src/main/java/com/example/dogs/repository/DogRepository.java
+++ src/main/java/com/example/dogs/repository/DogRepository.java
@@ -1,7 +1,12 @@
 package com.example.dogs.repository;
 
 import com.example.dogs.models.Dog;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface DogRepository extends CrudRepository<Dog, Long> {
+    @Query("select d.id, d.name from Dog d")
+    List<String> findAllName();
 }
--- src/main/java/com/example/dogs/resources/DogResource.java
+++ src/main/java/com/example/dogs/resources/DogResource.java
@@ -1,7 +1,41 @@
 package com.example.dogs.resources;
 
+import com.example.dogs.models.Dog;
+import com.example.dogs.service.DogService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/dogs")
 public class DogResource {
+
+    private DogService dogService;
+
+    public DogResource(DogService dogService) {
+        this.dogService = dogService;
+    }
+
+    @GetMapping("/breed")
+    public ResponseEntity<?> getDogBreeds() {
+        return new ResponseEntity<>( dogService.retrieveDogBreed(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Dog> findDog(@PathVariable(value = "id") Long id) {
+        return new ResponseEntity<>(dogService.retrieveDogById(id), HttpStatus.NOT_FOUND);
+    }
+
+    @GetMapping("/name")
+    public ResponseEntity<?> getDogNames() {
+        return new ResponseEntity<>(dogService.retrieveDogNames(), HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity<?> getDogs() {
+        return ResponseEntity.ok(dogService.retrieveDogs());
+    }
 }
